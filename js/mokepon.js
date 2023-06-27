c229af8202b427c018293932ecb4d573d756e8ac
Jose {Andres} Prendiz <jprendiz@gmail.com>
2023-06-26 22:02:53 -0600

    Obstaculos y colisiones

--- js/mokepon.js
+++ js/mokepon.js
@@ -329,8 +329,6 @@ function aleatorio(min, max) {
 }
 
 function pintarCanvas() {
-
-
     mascotaJugadorObjeto.x += mascotaJugadorObjeto.velocidadX
     mascotaJugadorObjeto.y += mascotaJugadorObjeto.velocidadY
     lienzo.clearRect(0,0, mapa.width, mapa.height)
@@ -345,6 +343,12 @@ function pintarCanvas() {
     hipodogeEnemigo.pintarMokepon()
     capipepoEnemigo.pintarMokepon()
     ratigueyaEnemigo.pintarMokepon()
+
+    if (mascotaJugadorObjeto.velocidadX != 0 || mascotaJugadorObjeto.velocidadY != 0) {
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+    }
 }
 
 function moverDerecha() {
@@ -410,4 +414,29 @@ function obtenerObjetoMascota() {
         }
     }
 }
+
+function revisarColision(enemigo) {
+    const arribaEnemigo = enemigo.y
+    const abajoEnemigo = enemigo.y + enemigo.alto
+    const derechaEnemigo = enemigo.x + enemigo.ancho
+    const izquierdaEnemigo = enemigo.x
+
+    const arribaMascota = mascotaJugadorObjeto.y
+    const abajoMascota = mascotaJugadorObjeto.y + mascotaJugadorObjeto.alto
+    const derechaMascota = mascotaJugadorObjeto.x + mascotaJugadorObjeto.ancho
+    const izquierdaMascota = mascotaJugadorObjeto.x
+
+    if (
+        abajoMascota < arribaEnemigo ||
+        arribaMascota > abajoEnemigo ||
+        derechaMascota < izquierdaEnemigo ||
+        izquierdaMascota > derechaEnemigo
+    ) {
+        return
+    } 
+
+    detenerMovimiento()
+    alert('Hay colisión ' + enemigo.nombre)
+}
+
 window.addEventListener('load', iniciarJuego)

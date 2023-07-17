bb8bf2475b1de04f41b1b4fd7bbb370635ef4aa1
Jose {Andres} Prendiz <jprendiz@gmail.com>
2023-07-17 10:26:49 -0600

    Batalla entre jugadores

--- index.js
+++ index.js
@@ -3,78 +3,95 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(cors())  // deshabilitar errores relacionados con cors
-app.use(express.json())  // habilitar capacidad de enviar y recibir json
+app.use(cors())
+app.use(express.json())
 
 const jugadores = []
 
 class Jugador {
-    constructor(id) {
-        this.id = id
-    }
+  constructor(id) {
+    this.id = id
+  }
 
-    asignarMokepon(mokepon) {
-        this.mokepon = mokepon
-    }
+  asignarMokepon(mokepon) {
+    this.mokepon = mokepon
+  }
 
-    actualizarPosicion(x, y) {
-        this.x = x
-        this.y = y
-    }
+  actualizarPosicion(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  asignarAtaques(ataques) {
+    this.ataques = ataques
+  }
 }
 
 class Mokepon {
-    constructor(nombre) {
-        this.nombre = nombre
-    }
+  constructor(nombre) {
+    this.nombre = nombre
+  }
 }
-//le decimos a express que cuando en el URL raíz (/) reciba una petición responda 'Hola'
-app.get("/unirse", (req, res)=>{
-    const id = `${Math.random()}`
 
-    const jugador = new Jugador(id)
+app.get("/unirse", (req, res) => {
+  const id = `${Math.random()}`
 
-    jugadores.push(jugador)
+  const jugador = new Jugador(id)
 
-    res.setHeader("Access-Control-Allow-Origin", "*")
+  jugadores.push(jugador)
 
-    res.send(id)
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  
+  res.send(id)
 })
 
-app.post("/mokepon/:jugadorId", (req, res)=> {
-    
-    const jugadorId = req.params.jugadorId || ""
-    const nombre = req.body.mokepon || ""
-    const mokepon = new Mokepon(nombre)
-    const jugadorIndex = jugadores.findIndex((jugador)=> jugadorId === jugador.id)
-    
-    if (jugadorIndex >= 0) {
-        jugadores[jugadorIndex].asignarMokepon(mokepon)
-    }
-    
-    console.log(jugadores);
-    console.log(jugadorId);
-    res.end()
+app.post("/mokepon/:jugadorId", (req, res) => {
+  const jugadorId = req.params.jugadorId || ""
+  const nombre = req.body.mokepon || ""
+  const mokepon = new Mokepon(nombre)
+  
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarMokepon(mokepon)
+  }
+  
+  console.log(jugadores)
+  console.log(jugadorId)
+  res.end()
 })
 
 app.post("/mokepon/:jugadorId/posicion", (req, res) => {
-    const jugadorId = req.params.jugadorId || ""
-    const x = req.body.x || 0
-    const y = req.body.y || 0
+  const jugadorId = req.params.jugadorId || ""
+  const x = req.body.x || 0
+  const y = req.body.y || 0
 
-    const jugadorIndex = jugadores.findIndex((jugador)=> jugadorId === jugador.id)
-    
-    if (jugadorIndex >= 0) {
-        jugadores[jugadorIndex].actualizarPosicion(x, y)
-    }
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
 
-    const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].actualizarPosicion(x, y)
+  }
 
-    res.send({
-        enemigos
-    })
-}) 
-//que escuche constantemente en el puerto 8080 para que cuando escuche una petición pueda responderla
+  const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+
+  res.send({
+    enemigos
+  })
+})
+
+app.post("/mokepon/:jugadorId/ataques", (req, res) => {
+  const jugadorId = req.params.jugadorId || ""
+  const ataques = req.body.ataques || []
+  
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarAtaques(ataques)
+  }
+
+  res.end()
+})
+
 app.listen(8080, () => {
-    console.log("Servidor funcionando");
+  console.log("Servidor funcionando")
 })

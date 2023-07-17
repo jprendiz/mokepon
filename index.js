2733137997ca7cd4f1b35a3f3a423321731a1c4f
Jose {Andres} Prendiz <jprendiz@gmail.com>
2023-07-17 12:08:11 -0600

    juego-completo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 
 const app = express()
 
+app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 
@@ -101,5 +102,5 @@ app.get("/mokepon/:jugadorId/ataques", (req, res) => {
 })
 
 app.listen(8080, () => {
-  console.log("Servidor funcionando")
+  console.log("Run Server")
 })
